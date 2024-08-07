64c5f30a67426f5f65ed9be30a8d817e56451cc6
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-08-07 09:22:12 -0300

    remove venv and add crud routes

--- app.py
+++ app.py
@@ -1,15 +1,89 @@
-from flask import Flask
+''''
+    CRUD
+    FOR
+    TASK
+    LIST
+'''
+
+from flask import Flask, request, jsonify
+from models.task import Task
 
 # __name__ = "main"
 app = Flask(__name__)
 
-@app.route("/tasks")
-def hello_world():
-    return "Hello world!"
 
-@app.route("/about")
-def about():
-    return "About"
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+
+    new_task = Task(id=task_id_control , title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+
+    tasks.append(new_task)
+    
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    # task_list = []
+    # for task in tasks:
+    #     task_list.append(task.to_dict())
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+
+    print(output)
+
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+
+    return jsonify({"message": "Tarefa não encontrada."}), 404 
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+
+    if task == None:
+        return jsonify({"message": "Tarefa não encontrada."}), 404
+    
+    data = request.get_json()
+    task.title = data['title'],
+    task.description = data['description']
+    task.completed = data['completed']
+
+    print(tasks)
+
+    return jsonify({"message": "Tarefa atualizada com sucesso encontrada."}) 
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({"message": "Tarefa não encontrada."}), 404
+
+    tasks.remove(task)        
+    return jsonify({"message": "Tarefa removida com sucesso."})
+
 
 # modo de executar manual (dev local), __name__ sempre sera main
 if __name__ == "__main__":
